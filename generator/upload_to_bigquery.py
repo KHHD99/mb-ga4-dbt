ab7c5f0b2413c58960ede7829d6ff648c9e0bbc7
KHHD99 <117235692+KHHD99@users.noreply.github.com>
2026-07-23 23:16:23 +0200

    Update upload_to_bigquery.py

--- generator/upload_to_bigquery.py
+++ generator/upload_to_bigquery.py
@@ -1,1 +1,19 @@
-to do
+from google.cloud import bigquery
+from schema import schema
+
+client = bigquery.Client()
+
+table = "khhd-473723.ga4_export.events_20260722"
+
+job = client.load_table_from_json(
+    rows,
+    table,
+    job_config=bigquery.LoadJobConfig(
+        schema=schema,
+        write_disposition="WRITE_TRUNCATE"
+    )
+)
+
+job.result()
+
+print("Done")
